b0d5fe113512a5130969b3d81393a1f11f8e9003
Andres <andresdt@pm.me>
2022-03-08 17:16:16 +0100

    Task 036: Done

---
+++ src/main/java/acme/entities/systemconfiguration/SystemConfiguration.java
@@ -0,0 +1,43 @@
+package acme.entities.systemconfiguration;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class SystemConfiguration extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long		serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotNull
+	protected String				currency;
+	
+	@NotNull
+	protected String 				acceptedCurrencies;
+
+	@NotNull
+	protected String				strongSpamTerms;
+
+	@NotNull
+	protected Double				strongSpamThreshold;
+	
+	@NotNull
+	protected String				weakSpamTerms;
+	
+	@NotNull
+	protected Double				weakSpamThreshold;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
