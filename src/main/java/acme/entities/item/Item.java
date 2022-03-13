8d882977e5631b549241efca4f1d0ed14fa5c88a
alecarnun <alecarnun@alum.us.es>
2022-03-13 20:32:46 +0100

    Task 041: Done

---
+++ src/main/java/acme/entities/item/Item.java
@@ -0,0 +1,57 @@
+package acme.entities.item;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.AbstractEntity;
+
+public class Item extends AbstractEntity{
+	
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max=101)
+	protected String name;
+				
+	@NotNull
+	@Column(unique=true)
+	@Pattern(regexp="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$",message = "default.error.conversion")
+	protected String code;
+				
+	@NotBlank
+	@Length(max=101)
+	protected String technology;
+				
+	@NotBlank
+	@Length(max=256)
+	protected String description;
+				
+	@NotNull
+	@PositiveOrZero
+	protected Money retailPrice;
+				
+	@URL
+	protected String info;
+	
+	@Enumerated(EnumType.STRING)
+	protected ItemType itemType;
+
+	// Derived attributes ----------------------------------------------------
+
+
+	// Relationships ----------------------------------------------------
+
+}
