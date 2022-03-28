52fc1a5d237d43e082dc8d83323019370bae682b
Pablo Jose Robledo Campa <pablorcmail@gmail.com>
2022-03-28 12:17:25 +0200

    Task 069: In Progress

--- src/main/java/acme/features/patron/dashboard/PatronDashboardController.java
+++ src/main/java/acme/features/patron/dashboard/PatronDashboardController.java
@@ -2,6 +2,8 @@ package acme.features.patron.dashboard;
 
 
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -10,19 +12,19 @@ import acme.framework.controllers.AbstractController;
 import acme.roles.Patron;
 
 @Controller
-public class PatronDashboardController extends AbstractController<Patron, PatronDashboard>{
+public class PatronDashboardController extends AbstractController<Patron, PatronDashboard> {
 
 	// Internal state ---------------------------------------------------------
-	
+
 	@Autowired
 	protected PatronDashboardShowService showService;
 
-
 	// Constructors -----------------------------------------------------------
-	
+
+
+	@PostConstruct
 	protected void initialise() {
-		
 		super.addCommand("show", this.showService);
 	}
-	
+
 }
--- src/main/java/acme/features/patron/dashboard/PatronDashboardShowService.java
+++ src/main/java/acme/features/patron/dashboard/PatronDashboardShowService.java
@@ -3,46 +3,47 @@ package acme.features.patron.dashboard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import acme.entities.patronages.Patronage;
+import acme.forms.PatronDashboard;
 import acme.framework.components.models.Model;
 import acme.framework.controllers.Request;
 import acme.framework.services.AbstractShowService;
 import acme.roles.Patron;
 
 @Service
-public class PatronDashboardShowService implements AbstractShowService<Patron, Patronage> {
+public class PatronDashboardShowService implements AbstractShowService<Patron, PatronDashboard> {
 
 	// Internal state ---------------------------------------------------------
-	
+
 	@Autowired
 	protected PatronDashboardRepository repository;
-	
-	
+
+	// AbstractShowService<Patron, PatronDashboard> interface ----------------
+
+
 	@Override
-	public boolean authorise(final Request<Patronage> request) {
+	public boolean authorise(final Request<PatronDashboard> request) {
 		assert request != null;
-		
+
 		return true;
 	}
 
 	@Override
-	public Patronage findOne(final Request<Patronage> request) {
+	public PatronDashboard findOne(final Request<PatronDashboard> request) {
+		assert request != null;
+
 		return null;
-		//TODO
 	}
 
 	@Override
-	public void unbind(final Request<Patronage> request, final Patronage entity, final Model model) {
+	public void unbind(final Request<PatronDashboard> request, final PatronDashboard entity, final Model model) {
 		assert request != null;
 		assert entity != null;
 		assert model != null;
-		
-		request.unbind(entity, model, 
-			"numberOfProposedPatronages","numberOfAcceptedPatronages", 
-			"numberOfDeniedPatronages", "stastBudgetofProposedPatronages",
-			"stastBudgetofAcceptedPatronages", "stastBudgetofDeniedPatronages");
-		
+
+		request.unbind(entity, model, //
+			"averageNumberOfJobsPerEmployer", "averageNumberOfApplicationsPerWorker", // 
+			"avegageNumberOfApplicationsPerEmployer", "ratioOfPendingApplications", //
+			"ratioOfRejectedApplications", "ratioOfAcceptedApplications");
 	}
-	
-	
+
 }
