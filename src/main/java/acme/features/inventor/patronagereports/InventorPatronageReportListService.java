56375d94b356a9c51c61aab9af74fc14121eeb7d
Pablo Jose Robledo Campa <pablorcmail@gmail.com>
2022-04-18 20:21:30 +0200

    Task 066: Done

---
+++ src/main/java/acme/features/inventor/patronagereports/InventorPatronageReportListService.java
@@ -0,0 +1,56 @@
+package acme.features.inventor.patronagereports;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.patronagereports.PatronageReport;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.services.AbstractListService;
+import acme.roles.Inventor;
+
+@Service
+public class InventorPatronageReportListService implements AbstractListService<Inventor,PatronageReport> {
+
+	// Internal state ---------------------------------------------------------
+	
+	@Autowired
+	protected InventorPatronageReportRepository repository;
+	
+	@Override
+	public boolean authorise(final Request<PatronageReport> request) {
+		assert request != null;
+
+		return true;
+	}
+
+
+	@Override
+	public Collection<PatronageReport> findMany(final Request<PatronageReport> request) {
+		assert request != null;
+
+		final int inventorId = request.getPrincipal().getActiveRoleId();
+		
+		return this.repository.findAllPatronageReport().stream().filter(x -> x.getPatronage().getInventor().getId()==inventorId).collect(Collectors.toList());
+	}
+
+
+	@Override
+	public void unbind(final Request<PatronageReport> request, final PatronageReport entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "sequenceNumber", "creationTime", "memorandum", "info", "patronage.status", "patronage.legalStuff", "patronage.budget", "patronage.creationDate", "patronage.startDate", "patronage.endDate", "patronage.info");
+	}
+	
+	
+	
+	
+	
+	
+	
+}
