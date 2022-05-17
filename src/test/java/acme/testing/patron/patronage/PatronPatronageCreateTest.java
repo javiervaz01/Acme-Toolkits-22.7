56eb0e5f4ac710b4556692e73ba230c5b3dba2e4
alecarnun <alecarnun@alum.us.es>
2022-05-17 13:59:13 +0200

    Task 087: in progress

--- src/test/java/acme/testing/patron/patronage/PatronPatronageCreateTest.java
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageCreateTest.java
@@ -10,7 +10,7 @@ public class PatronPatronageCreateTest extends TestHarness {
 @ParameterizedTest
 @CsvFileSource(resources = "/patron/patronage/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 @Order(10)
-public void positiveTest(final int inventorRecordIndex, final String status, final String code, final String legalStuff, final String budget, final String exchange, final String startDate, final String endDate, final String info) {
+public void positiveTest(final int inventorRecordIndex, final int recordIndex, final String status, final String code, final String legalStuff, final String budget, final String exchange, final String startDate, final String endDate, final String info) {
 	super.signIn("patron1", "patron1");
 
 	super.clickOnMenu("Patron", "Propose a patronage");
@@ -21,7 +21,6 @@ public void positiveTest(final int inventorRecordIndex, final String status, fin
 	super.fillInputBoxIn("code", code);
 	super.fillInputBoxIn("legalStuff", legalStuff);
 	super.fillInputBoxIn("budget", budget);
-	super.fillInputBoxIn("exchange", exchange);
 	super.fillInputBoxIn("startDate", startDate);
 	super.fillInputBoxIn("endDate", endDate);
 	super.fillInputBoxIn("info", info);
@@ -30,25 +29,31 @@ public void positiveTest(final int inventorRecordIndex, final String status, fin
 	super.clickOnMenu("Patron", "List my patronages");
 	super.checkListingExists();
 	super.sortListing(0, "asc");
-	//super.checkColumnHasValue(recordIndex, 0, reference);
-	//super.checkColumnHasValue(recordIndex, 1, deadline);
-	//super.checkColumnHasValue(recordIndex, 2, title);
-
-	//super.checkFormExists();
-	//super.checkInputBoxHasValue("reference", reference);
-	//super.checkInputBoxHasValue("title", title);
-	//super.checkInputBoxHasValue("deadline", deadline);
-	//super.checkInputBoxHasValue("salary", salary);
-	//super.checkInputBoxHasValue("score", score);
-	//super.checkInputBoxHasValue("moreInfo", moreInfo);
-	//super.checkInputBoxHasValue("description", description);
-
-	//super.clickOnButton("Duties");
-
-	//super.checkListingExists();
-	//super.checkListingEmpty();
-
-	//super.signOut();
+	
+	super.checkColumnHasValue(recordIndex, 0, status);
+	super.checkColumnHasValue(recordIndex, 1, code);
+	super.checkColumnHasValue(recordIndex, 2, budget);
+	
+	super.clickOnListingRecord(recordIndex);
+	super.checkFormExists();
+	super.checkInputBoxHasValue("status", status);
+	super.checkInputBoxHasValue("code", code);
+	super.checkInputBoxHasValue("legalStuff", legalStuff);
+	super.checkInputBoxHasValue("budget", budget);
+	//super.checkInputBoxHasValue("creationDate", creationDate);
+	super.checkInputBoxHasValue("exchange", exchange);
+	super.checkInputBoxHasValue("startDate", startDate);
+	super.checkInputBoxHasValue("endDate", endDate);
+	super.checkInputBoxHasValue("info", info);
+	
+	//super.checkInputBoxHasValue("inventorName", patronIdentityName);
+	//super.checkInputBoxHasValue("inventorSurname", patronIdentitySurname);
+	//super.checkInputBoxHasValue("inventorEmail", patronIdentityEmail);
+	//super.checkInputBoxHasValue("inventorCompany", patronCompany);
+	//super.checkInputBoxHasValue("inventorStatement", patronStatement);
+	//super.checkInputBoxHasValue("inventorInfo", patronInfo);
+	
+	super.signOut();
 }
 
 }
