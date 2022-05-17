0947cac12a1f0f528e928ff578c4ca10fd283ec3
Pablo Núñez Moreno <panumo123@gmail.com>
2022-05-17 09:24:06 +0200

    Task 081: done (commented spam filter)

---
+++ src/main/java/acme/features/any/chirp/AnyChirpCreationRepository.java
@@ -0,0 +1,9 @@
+package acme.features.any.chirp;
+
+import org.springframework.stereotype.Repository;
+
+import acme.framework.repositories.AbstractRepository;
+
+@Repository
+public interface AnyChirpCreationRepository extends AbstractRepository{
+}
