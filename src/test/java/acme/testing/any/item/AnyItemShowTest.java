b0247d0dbfb68a30ee5b671a0effffaedfb8b605
Francisco Javier Vázquez Monge <69580710+javiervaz01@users.noreply.github.com>
2022-05-20 00:07:09 +0200

    Task 058-t1: done

---
+++ src/test/java/acme/testing/any/item/AnyItemShowTest.java
@@ -0,0 +1,42 @@
+package acme.testing.any.item;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class AnyItemShowTest extends TestHarness {
+
+	// Lifecycle management ---------------------------------------------------
+
+	// Test cases -------------------------------------------------------------
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/any/items/show.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void positiveTest(final int key, final String name, final String code, final String technology,
+			final String description, final String retailPrice, final String info, final String type) {
+
+		super.clickOnMenu("Any", "List items");
+		super.checkListingExists();
+		super.sortListing(1, "asc");
+
+		super.checkColumnHasValue(key, 0, name);
+		super.checkColumnHasValue(key, 1, code);
+		super.checkColumnHasValue(key, 2, technology);
+
+		super.clickOnListingRecord(key);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("name", name);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("technology", technology);
+		super.checkInputBoxHasValue("description", description);
+		super.checkInputBoxHasValue("retailPrice", retailPrice);
+		super.checkInputBoxHasValue("info", info);
+		super.checkInputBoxHasValue("type", type);
+	}
+
+	// Ancillary methods ------------------------------------------------------
+
+}
